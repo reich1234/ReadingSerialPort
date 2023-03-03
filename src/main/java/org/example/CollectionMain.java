40043624a930c83fd579249b4de1b4929e143d4a
reich1234 <jwu@reich-germany.de>
2023-03-03 08:43:49 +0100

    update code

--- src/main/java/org/example/CollectionMain.java
+++ src/main/java/org/example/CollectionMain.java
@@ -3,16 +3,14 @@ package org.example;
 import java.util.Arrays;
 import com.serotonin.modbus4j.ModbusFactory;
 import com.serotonin.modbus4j.ModbusMaster;
-import com.serotonin.modbus4j.code.DataType;
-import com.serotonin.modbus4j.locator.NumericLocator;
-import com.serotonin.modbus4j.code.RegisterRange;
+import com.serotonin.modbus4j.ProcessImage;
 import com.serotonin.modbus4j.exception.ModbusInitException;
 import com.serotonin.modbus4j.exception.ModbusTransportException;
-import com.serotonin.modbus4j.msg.ReadDiscreteInputsRequest;
-import com.serotonin.modbus4j.msg.ReadDiscreteInputsResponse;
-import com.serotonin.modbus4j.msg.ReadHoldingRegistersRequest;
-import com.serotonin.modbus4j.msg.ReadHoldingRegistersResponse;
+import com.serotonin.modbus4j.msg.*;
 import com.serotonin.modbus4j.serial.SerialPortWrapper;
+import org.example.SerialPortWrapperImpl;
+
+import static com.serotonin.modbus4j.serial.SerialPortWrapper.*;
 
 /**
  * 通过串口解析MODBUS协议
@@ -22,11 +20,27 @@ public class CollectionMain {
     // 设定MODBUS网络上从站地址
     private final static int SLAVE_ADDRESS = 1;
     //串行波特率
-    private final static int BAUD_RATE = 9600;
+    private final static int BAUD_RATE = 115200;
 
     public static void main(String[] args) {
+//        SerialPortWrapper serialParameters = new
+//                SerialPortWrapperImpl("/dev/ttyS2", BAUD_RATE, 8, 1, 0, 0, 0);
+//        /* 创建ModbusFactory工厂实例 */
+//        ModbusFactory modbusFactory = new ModbusFactory();
+//        /* 创建ModbusMaster实例 */
+//        ModbusMaster master = modbusFactory.createRtuMaster(serialParameters);
+//        /* 初始化 */
+//        try {
+//            master.init();
+//            readHoldingRegistersTest(master, SLAVE_ADDRESS, 256, 1);
+//        } catch (ModbusInitException e) {
+//            e.printStackTrace();
+//        } finally {
+//            master.destroy();
+//        }
+
         SerialPortWrapper serialParameters = new
-                SerialPortWrapperImpl("/dev/ttyS2", BAUD_RATE, 8, 1, 0, 0, 0);
+                SerialPortWrapperImpl("/dev/ttyS0", BAUD_RATE, 8, 1, 1, 0, 0);
         /* 创建ModbusFactory工厂实例 */
         ModbusFactory modbusFactory = new ModbusFactory();
         /* 创建ModbusMaster实例 */
@@ -34,28 +48,14 @@ public class CollectionMain {
         /* 初始化 */
         try {
             master.init();
-            readHoldingRegistersTest(master, SLAVE_ADDRESS, 256, 1);
+            //readDiscreteInputTest(master, SLAVE_ADDRESS, 0, 20);
+            readCoil(master,SLAVE_ADDRESS, 0, 20);
+            //readHoldingRegistersTest(master, SLAVE_ADDRESS, 256, 1);
         } catch (ModbusInitException e) {
             e.printStackTrace();
         } finally {
             master.destroy();
         }
-
-        /*SerialPortWrapper serialParameters = new
-                SerialPortWrapperImpl("/dev/ttyS0", BAUD_RATE, 8, 1, 1, 0, 0);
-        *//* 创建ModbusFactory工厂实例 *//*
-        ModbusFactory modbusFactory = new ModbusFactory();
-        *//* 创建ModbusMaster实例 *//*
-        ModbusMaster master = modbusFactory.createRtuMaster(serialParameters);
-        *//* 初始化 *//*
-        try {
-            master.init();
-            readDiscreteInputTest(master, SLAVE_ADDRESS, 0, 20);
-        } catch (ModbusInitException e) {
-            e.printStackTrace();
-        } finally {
-            master.destroy();
-        }*/
     }
 
     /**
@@ -65,23 +65,18 @@ public class CollectionMain {
      * @param start 起始地址的偏移量
      * @param len 待读寄存器的个数
      */
-    public static void readHoldingRegistersTest(ModbusMaster master, int slaveId, int start, int len) {
+    private static void readHoldingRegistersTest(ModbusMaster master, int slaveId, int start, int len) {
         try {
-
-
-
             ReadHoldingRegistersRequest request = new ReadHoldingRegistersRequest(slaveId, start, len);
             ReadHoldingRegistersResponse response = (ReadHoldingRegistersResponse)master.send(request);
-            NumericLocator locator = new NumericLocator(
-                    slaveId, RegisterRange.HOLDING_REGISTER, start, DataType.FOUR_BYTE_FLOAT);
             if (response.isException()) {
                 System.out.println("Exception response: message=" + response.getExceptionMessage());
             } else {
-
-                byte[] list = response.getData();
-                Number result = locator.bytesToValue(list, len);
-                System.out.print(result);
-
+                System.out.println(Arrays.toString(response.getShortData()));
+                short[] list = response.getShortData();
+                for (int i = 0; i < list.length; i++) {
+                    System.out.print(list[i] + " ");
+                }
             }
         } catch (ModbusTransportException e) {
             e.printStackTrace();
@@ -90,9 +85,34 @@ public class CollectionMain {
 
     public static void readDiscreteInputTest(ModbusMaster master, int slaveId, int start, int len) {
         try {
+
             ReadDiscreteInputsRequest request = new ReadDiscreteInputsRequest(slaveId, start, len);
+            byte list2 = request.getFunctionCode();
+            System.out.println(list2);
+            // how to get response from ReadDecreteInputsRequest with this library?
             ReadDiscreteInputsResponse response = (ReadDiscreteInputsResponse) master.send(request);
             boolean[] list = response.getBooleanData();
+
+            for (int i = 0; i < list.length; i++) {
+                System.out.print(list[i] + " ");
+            }
+        } catch (ModbusTransportException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void readCoil(ModbusMaster master, int slaveId, int start, int len) {
+        try {
+
+            ReadCoilsRequest request = new ReadCoilsRequest(slaveId, start, len);
+            byte list2 = request.getFunctionCode();
+            System.out.println(list2);
+            // how to get response from ReadDecreteInputsRequest with this library?
+            ReadCoilsResponse response = (ReadCoilsResponse) master.send(request);
+
+            boolean[] list = response.getBooleanData();
+
             for (int i = 0; i < list.length; i++) {
                 System.out.print(list[i] + " ");
             }
@@ -103,4 +123,3 @@ public class CollectionMain {
     }
 
 }
-
